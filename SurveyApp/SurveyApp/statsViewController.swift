8c658efaf234f9cb49d2195bc8f2453806dbcba2
Sean12697 <seandavidomahoney@gmail.com>
2019-03-21 17:38:10 +0000

    Send Email In Progress

--- SurveyApp/SurveyApp/statsViewController.swift
+++ SurveyApp/SurveyApp/statsViewController.swift
@@ -8,19 +8,29 @@
 
 import UIKit
 import CoreData
+import MessageUI
 
-class statsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+class statsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var dataArr : [String] = [];
     let diff : [String] = ["Very Easy", "Easy", "Okay", "Difficult", "Very Hard"];
     let simpleTableIdentifier = "SimpleTableIdentifier"
     @IBOutlet weak var dataTable: UITableView!
+    // For CSV
+    let fileName = "Exports.csv"
+    let path = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("Exports.csv")
+    var csvText = "ID,Age,Q1,Q2,Q3,Lat,Long\n"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         dataArr = getData()
         dataTable.dataSource = self
         dataTable.delegate = self
+    }
+    
+    @IBAction func sendEmailClick(_ sender: Any) {
+        print(csvText)
+        sendEmail()
     }
     
     func getData() -> [String] {
@@ -32,13 +42,16 @@ class statsViewController: UIViewController, UITableViewDataSource, UITableViewD
         do {
             let result = try managedContext.fetch(fetchRequest)
             for item in result as! [NSManagedObject] {
+                // For Stat View
                 let id:String = item.value(forKey: "id") as! String;
                 let age:String = String(getAge(birthday: item.value(forKey: "dob") as! Date))
                 let used:Bool = item.value(forKey: "q1") as! Bool;
                 let usedString:String = (used) ? "Uses iPads for \(item.value(forKey: "q2a") as! String)" : "Finds iPads \(diff[item.value(forKey: "q2b") as! Int]) to use";
                 let consider:String = (item.value(forKey: "q3") as! Bool) ? "will" : "won't";
                 data.append("\(id) - Age: \(age), \(usedString) and \(consider) consider using an iPad again in the future");
-                // item.value(forKey: "id") as? String
+                // For CSV
+                let newLine = "\(id),\(age),\(used),\(used ? item.value(forKey: "q2a") as! String : diff[item.value(forKey: "q2b") as! Int]),\(item.value(forKey: "q3") as! Bool)\n"
+                csvText += newLine
             }
         } catch {
             print("Failed");
@@ -46,6 +59,50 @@ class statsViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         return data;
     }
+    
+    func sendEmail() {
+        do {
+        try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.setSubject("Survey Result Export")
+            mailComposer.setMessageBody("Hi,\n\nAttached is the .csv exports of the results from the questionaire.", isHTML: false)
+//            mailComposer.addAttachmentData(Data(context: NSData(contentsOfURL: path!)), mimeType: "text/csv", fileName: fileName)
+            mailComposer.mailComposeDelegate = (self as! MFMailComposeViewControllerDelegate)
+            self.present(mailComposer, animated: true
+                , completion: nil)
+        } else {
+            print("Email is not configured in settings app or we are not able to send an email")
+        }
+        } catch {
+            print("Failed to create file")
+            print("\(error)")
+        }
+//        do {
+//            try csvText.write(to: path!, atomically: true, encoding: String.Encoding.utf8)
+//
+//
+//
+////            if mailComposeController.canSendMail() {
+////                let emailController = mailComposeController()
+////                emailController.mailComposeDelegate = self
+////                emailController.setToRecipients([])
+////                emailController.setSubject("Survey Result Export")
+////                emailController.setMessageBody("Hi,\n\nAttached is the .csv exports of the results from the questionaire.", isHTML: false)
+////
+////                emailController.addAttachmentData(NSData(contentsOfURL: path)!, mimeType: "text/csv", fileName: fileName)
+////                presentViewController(emailController, animated: true, completion: nil)
+////            }
+//
+//        } catch {
+//            print("Failed to create file")
+//            print("\(error)")
+//        }
+    }
+    
+//    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+//        controller.dismissViewControllerAnimated(true, completion: nil)
+//    }
     
     func getAge(birthday: Date) -> Int {
         let now = Date()
